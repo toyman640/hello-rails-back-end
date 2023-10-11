9bc83a0dbf912f4c77c1807c099a91a020e67468
toyman640 <falakoadebayor@gmail.com>
2023-10-11 20:05:13 +0100

    Fix linters errors

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,6 +1,12 @@
-class Api::V1::GreetingsController < ApplicationController
-  def index
-    @random_greeting = Greeting.order('RANDOM()').first
-    render json: { greeting: @random_greeting&.message }
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class GreetingsController < ApplicationController
+      def index
+        @random_greeting = Greeting.order('RANDOM()').first
+        render json: { greeting: @random_greeting&.message }
+      end
+    end
   end
 end
